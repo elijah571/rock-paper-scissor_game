66181f143114328f49aedc1c5b1dcd3627df7995
elijah751 <elijahfx43@gmail.com>
2024-03-03 22:40:02 +0100

    created random move with maths.random function. save the random number is computerMove variable, inside a function and return computer move;

---
+++ app.js
@@ -0,0 +1,13 @@
+function pickComputerMove() {
+    const randomNumber = Math.random();
+    let computerMove = '';
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+        computerMove = 'rock';
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        computerMove = 'paper';
+    } else if (randomNumber>= 2/3 && randomNumber < 1) {
+        computerMove = 'scissors';
+    }
+    return computerMove;
+    console.log(computerMove);
+}
